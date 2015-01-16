016082b53c6759bd2b5460c2e9cf8ab08ace3e18
Brian Underwood <ml+github@semi-sentient.com>
2015-01-16 08:01:09 +0200

    Adding redis-activesupport

--- app/models/url_cache.rb
+++ app/models/url_cache.rb
@@ -2,7 +2,7 @@ class UrlCache
   CACHE = if ENV['REDISCLOUD_URL']
             ActiveSupport::Cache::RedisStore.new(ENV['REDISCLOUD_URL'])
           else
-            CACHE = ActiveSupport::Cache::FileStore.new('cache')
+            ActiveSupport::Cache::FileStore.new('cache')
           end
 
   def self.get_url_body(url, options = {expires_in: 1.month})
